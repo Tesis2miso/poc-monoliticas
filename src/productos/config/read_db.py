78d348bb127a88f9406ed886a19e8397f7c91f82
William Ravelo M <drummerwilliam@gmail.com>
2023-03-03 16:38:22 -0500

    timeout

--- src/productos/config/read_db.py
+++ src/productos/config/read_db.py
@@ -12,7 +12,7 @@ DB_READ_HOSTNAME = os.getenv('DB_READ_HOSTNAME', default="localhost")
 DB_READ_NAME = os.getenv('DB_READ_NAME', default="localhost")
 
 base = declarative_base()
-engine = sa.create_engine(f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_READ_HOSTNAME}/{DB_READ_NAME}')
+engine = sa.create_engine(f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_READ_HOSTNAME}/{DB_READ_NAME}', connect_args={'connect_timeout': 60})
 base.metadata.bind = engine
 session = orm.scoped_session(orm.sessionmaker())(bind=engine)
 base.metadata.create_all()

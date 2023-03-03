ec85e24df56707b6432e6002996a6cff88bacb40
abaron10 <af.baron10@uniandes.edu.co>
2023-03-02 19:19:22 -0500

    test events

--- src/ordenes/utils.py
+++ src/ordenes/utils.py
@@ -1,12 +1,12 @@
 import os
-
+import time
 PULSAR_ENV: str = 'BROKER_HOST'
 DB_ENV: str = 'DB_HOST'
 DB_USER_ENV: str = 'DB_USER'
 DB_DATABASE: str = 'DB_DATABASE'
 
 def broker_host():
-    return os.getenv(PULSAR_ENV, default="localhost")
+    return os.getenv(PULSAR_ENV, default="34.121.128.81")
 
 def db_host():
     return os.getenv(DB_ENV, default="34.68.216.107")
@@ -16,3 +16,6 @@ def db_user():
 
 def db_database():
     return os.getenv(DB_DATABASE, default="monoliticas")
+
+def time_millis():
+    return int(time.time() * 1000)

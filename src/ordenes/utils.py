66fdce9ca3e22beef3933a38bf1df7a9a3736201
William Ravelo M <drummerwilliam@gmail.com>
2023-03-08 12:20:25 -0500

    fix

--- src/ordenes/utils.py
+++ src/ordenes/utils.py
@@ -1,16 +1,16 @@
 import os
 import time
 PULSAR_ENV: str = 'BROKER_HOST'
-DB_ENV: str = 'DB_HOST'
+DB_HOSTNAME: str = 'DB_HOSTNAME'
 DB_USER_ENV: str = 'DB_USER'
 DB_DATABASE: str = 'DB_DATABASE'
-DB_READ_ENV: str = 'DB_READ_ENV'
+DB_READ_HOSTNAME: str = 'DB_READ_HOSTNAME'
 
 def broker_host():
     return os.getenv(PULSAR_ENV, default="34.121.128.81")
 
 def db_host():
-    return os.getenv(DB_ENV, default="34.68.216.107")
+    return os.getenv(DB_HOSTNAME, default="34.68.216.107")
 
 def db_user():
     return os.getenv(DB_USER_ENV, default="root")
@@ -20,7 +20,7 @@ def db_database():
 
 
 def db_host_replica():
-    return os.getenv(DB_READ_ENV, default="35.226.225.189")
+    return os.getenv(DB_READ_HOSTNAME, default="35.226.225.189")
 
 def db_user_replica():
     return os.getenv(DB_USER_ENV, default="root")

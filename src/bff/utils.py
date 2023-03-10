7099379d3fbf14f5c96fe27423aabe2f83f2a8e9
abaron10 <af.baron10@uniandes.edu.co>
2023-03-09 20:40:58 -0500

    order endpoints

--- src/bff/utils.py
+++ src/bff/utils.py
@@ -1,6 +1,27 @@
 import os
+import time
 
 PULSAR_ENV: str = 'BROKER_HOST'
+DB_HOSTNAME: str = 'DB_HOSTNAME'
+DB_USER_ENV: str = 'DB_USER'
+DB_DATABASE: str = 'DB_DATABASE'
+
 
 def broker_host():
     return os.getenv(PULSAR_ENV, default="34.121.128.81")
+
+
+def db_host():
+    return os.getenv(DB_HOSTNAME, default="34.68.216.107")
+
+
+def db_user():
+    return os.getenv(DB_USER_ENV, default="root")
+
+
+def db_database():
+    return os.getenv(DB_DATABASE, default="monoliticas")
+
+
+def time_millis():
+    return int(time.time() * 1000)
